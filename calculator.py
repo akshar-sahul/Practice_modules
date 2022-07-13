f59c93f1ce920b3b2cccaa399b679a03792c97b3
aksharsahul <aksharsahul07@gmail.com>
2022-07-13 16:45:42 +0530

    3rd commit

---
+++ calculator.py
@@ -0,0 +1,12 @@
+class calci:
+    def __init__(self,a,b):
+        self.a=a
+        self.b=b
+    def addition(self):
+        return self.a+self.b
+    def subtraction(self):
+        return self.a-self.b
+    def multiplication(self):
+        return self.a*self.b
+    def division(self):
+        return self.a*self.b
---
+++ operations.py
@@ -0,0 +1,4 @@
+from calculator import calci
+c=calci(a=34,b=45)
+print(f'addition: {c.addition()}')
+print(f'subtraction :{c.subtraction()}')    
